67c3bc0d0320bca2bf4dbbacedc3cd3313776c65
Hyeyoung Park <parhyeyo@sheridancollege.ca>
2022-06-21 13:36:25 -0400

    Add a class name, "hyeyoung" and add a comment

---
+++ src/hyeyoung.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hannah
+ */
+public class hyeyoung {
+    
+    public static void main(String[] args){
+    
+        System.out.println("Hello, my name is Hyeyoung Park");
+        
+    }
+    
+}
